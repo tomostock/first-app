1851ea4897569d5dc7225f199111a3daae12dc68
tomostock <tomos.tomomi@gmail.com>
2021-05-21 02:28:10 +0900

    test(21.5.21)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
-      {props.value}
+     test {props.value}
     </button>
   );
 }
